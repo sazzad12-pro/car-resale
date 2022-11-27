423400c3b87ce22ebe20b0e8a2ef3abdf67e0d35
sazzad <sajusajjad052@gmail.com>
2022-11-27 19:14:14 +0600

    add product update

--- src/page/Product/AddProduct.js
+++ src/page/Product/AddProduct.js
@@ -1,112 +1,211 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddProduct = () => {
-  const handleBuyerInfo = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const name = form.productName.value;
-    const reprice = form.reprice.value;
-    const orprice = form.orprice.value;
-    const location = form.location.value;
-    const year = form.year.value;
-    const condition = form.condition.value;
-    const category = form.category.value;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
-    const info = {
-      name,
-      reprice,
-      orprice,
-      location,
-      year,
-      condition,
-    };
-    fetch(`${process.env.REACT_APP_url}/product?name=${category}`, {
+  const imgHostKey = process.env.REACT_APP_imgbb;
+
+  const handleAddProduct = (data) => {
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?key=${imgHostKey}`;
+    fetch(url, {
       method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(info),
+      body: formData,
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((imgData) => {
+        if (imgData.success) {
+          const info = {
+            name: data.name,
+            img: imgData.data.url,
+            reprice: data.reprice,
+            orprice: data.orprice,
+            location: data.location,
+            year: data.year,
+            condition: data.condition,
+            describe: data.describe,
+            category: data.category,
+          };
+          fetch(`${process.env.REACT_APP_url}/product?name=${data.category}`, {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(info),
+          })
+            .then((res) => res.json())
+            .then((result) => {
+              toast.success("product add");
+              navigate("/");
+            });
+        }
+      });
   };
 
   return (
     <div>
       <h1>Add a product</h1>
-      <div className="text-center">
-        <form onSubmit={handleBuyerInfo}>
-          <input
-            type="text"
-            placeholder="Enter your product Name"
-            name="productName"
-            className="input input-bordered input-primary w-full max-w-xs mt-4"
-          />
-          <br />
-          <input
-            type="text"
-            placeholder="Enter your Resale price"
-            name="reprice"
-            className="input input-bordered input-primary w-full max-w-xs mt-4"
-          />
-          <br />
-          <input
-            type="text"
-            placeholder="Enter your Original price"
-            name="orprice"
-            className="input input-bordered input-primary w-full max-w-xs mt-4"
-          />
-          <br />
+      <div className="w-[50%] mx-auto">
+        <form onSubmit={handleSubmit(handleAddProduct)}>
+          <div className="form-control w-full max-w-x">
+            <input
+              type="text"
+              {...register("name", {
+                require: "name is required",
+              })}
+              className="input input-bordered input-primary w-full max-w-xs mt-4"
+              placeholder="Enter your Name"
+            />
+            {errors.name && (
+              <p className="text-red-500">{errors.name.message}</p>
+            )}
+          </div>
 
-          <input
-            type="text"
-            placeholder="Enter your product Location"
-            name="location"
-            className="input input-bordered input-primary w-full max-w-xs mt-4"
-          />
           <br />
-          <input
-            type="text"
-            placeholder="Year of purchase"
-            name="year"
-            className="input input-bordered input-primary w-full max-w-xs mt-4"
-          />
+          <div className="form-control w-full max-w-x">
+            {" "}
+            <input
+              type="text"
+              {...register("reprice", {
+                require: "Price is require",
+              })}
+              className="input input-bordered input-primary w-full max-w-xs mt-4"
+              placeholder="Enter your sell price"
+            />
+            {errors.reprice && (
+              <p className="text-red-500">{errors.reprice.message}</p>
+            )}
+          </div>
+
           <br />
-          <input
-            type="file"
-            name="img"
-            className="input input-bordered input-primary w-full max-w-xs mt-4"
-          />
+          <div className="form-control w-full max-w-x">
+            {" "}
+            <input
+              type="text"
+              {...register("orprice", {
+                require: "Price is require",
+              })}
+              className="input input-bordered input-primary w-full max-w-xs mt-4"
+              placeholder="Enter Your Original Price"
+            />
+            {errors.orprice && (
+              <p className="text-red-500">{errors.orprice.message}</p>
+            )}
+          </div>
+
           <br />
-          <select
-            name="condition"
-            className="select select-primary select-bordered w-full max-w-xs mt-4"
-          >
-            <option disabled selected>
-              Condition type
-            </option>
-            <option>Excellent</option>
-            <option>Good</option>
-            <option>Fair</option>
-          </select>
+          <div className="form-control w-full max-w-x">
+            <input
+              type="text"
+              {...register("location", {
+                require: "Price is require",
+              })}
+              className="input input-bordered input-primary w-full max-w-xs mt-4"
+              placeholder="Enter your location"
+            />
+            {errors.location && (
+              <p className="text-red-500">{errors.location.message}</p>
+            )}
+          </div>
+
           <br />
-          <select
-            name="category"
-            className="select select-primary select-bordered w-full max-w-xs mt-4"
-          >
-            <option disabled selected>
-              Product Category
-            </option>
-            <option>Microbus</option>
-            <option>Luxury car</option>
-            <option>Electric car</option>
-          </select>
+          <div className="form-control w-full max-w-x">
+            {" "}
+            <input
+              type="text"
+              {...register("year", {
+                require: "Price is require",
+              })}
+              className="input input-bordered input-primary w-full max-w-xs mt-4"
+              placeholder="how many day use "
+            />
+            {errors.year && (
+              <p className="text-red-500">{errors.year.message}</p>
+            )}
+          </div>
+
           <br />
-          <textarea
-            name="describe"
-            className="textarea w-full max-w-xs textarea-primary mt-4"
-            placeholder="describe"
-          ></textarea>
+          <div className="form-control w-full max-w-x">
+            {" "}
+            <input
+              type="file"
+              {...register("image", {
+                require: "Image is require",
+              })}
+              className="input input-bordered input-primary w-full max-w-xs mt-4"
+            />
+            {errors.image && (
+              <p className="text-red-500">{errors.image.message}</p>
+            )}
+          </div>
+
+          <br />
+          <div className="form-control w-full max-w-x">
+            {" "}
+            <select
+              {...register("condition", {
+                require: "Condition is require",
+              })}
+              className="select select-primary select-bordered w-full max-w-xs mt-4"
+            >
+              <option disabled selected>
+                Condition type
+              </option>
+              <option>Excellent</option>
+              <option>Good</option>
+              <option>Fair</option>
+            </select>
+            {errors.condition && (
+              <p className="text-red-500">{errors.condition.message}</p>
+            )}
+          </div>
+
+          <br />
+          <div className="form-control w-full max-w-x">
+            {" "}
+            <select
+              {...register("category", {
+                require: "Category is require",
+              })}
+              className="select select-primary select-bordered w-full max-w-xs mt-4"
+            >
+              <option disabled selected>
+                Product Category
+              </option>
+              <option>Microbus</option>
+              <option>Luxury car</option>
+              <option>Electric car</option>
+            </select>
+            {errors.category && (
+              <p className="text-red-500">{errors.category.message}</p>
+            )}
+          </div>
+
+          <br />
+          <div className="form-control w-full  text-center">
+            {" "}
+            <textarea
+              {...register("describe", {
+                require: "Describe is require",
+              })}
+              className="textarea w-full max-w-xs textarea-primary mt-4"
+              placeholder="describe"
+            ></textarea>
+            {errors.describe && (
+              <p className="text-red-500">{errors.describe.message}</p>
+            )}
+          </div>
+
           <br />
           <input
             type="submit"
